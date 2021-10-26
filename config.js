53ea6a71104d9e45098f12f8d03a1a37e170cc39
huk250699 <87457168+huk250699@users.noreply.github.com>
2021-10-26 21:46:11 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Nguyễn Khánh Huyền';
-const giftUrl = 'http://nodemy.vn';
+const giftUrl = 'https://www.facebook.com/';
 const eventName = 'Happy Birthday To Huyền 🥰';
 const titleCard = 'Tặng Huyền';
 const contentCard = 'Chúc Huyền sinh nhật vui vẻ, thành công trên con đường mình đã chọn và nhớ là luôn hạnh phúc và bình an nhé!';
